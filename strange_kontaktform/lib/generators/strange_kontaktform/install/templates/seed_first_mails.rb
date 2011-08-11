40ac9f45d94ff51706103a1a89cd34e085f1830b
Austin Strange <web@orangenwerk.com>
2011-08-11 03:08:40 +0200

    remove big sample-movie

--- strange_kontaktform/lib/generators/strange_kontaktform/install/templates/seed_first_mails.rb
+++ strange_kontaktform/lib/generators/strange_kontaktform/install/templates/seed_first_mails.rb
@@ -1,8 +1,6 @@
    
 
 
-# encoding: utf-8
-
 puts 'INFO: erstelle Kontakt-Seite'
 kontakt = Seite.create!       :name => 'Kontakt', 
                               :system_seite => true, 
@@ -45,14 +43,14 @@ puts 'INFO: Seite erstellt: ' << kontakt.name
  
 
 email_1 = KontaktFormular.create! :anrede => 'Herr', :name => 'fineline-Team', :email => 'cms@orangenwerk.com', :betreff => 'fineline geht online', 
-:inhalt => 'Wir fr&uuml;hen uns Ihnen mitteilen zu k&ouml;nnen, dass unser eigens f&uuml;r Sie entwickelstes CMS online ist.
+:inhalt => 'Wir frühen uns Ihnen mitteilen zu können, dass unser eigens für Sie entwickelstes CMS online ist.
 
 fineline-cms.com
 
 probieren Sie es aus!', :user_ip => '00.00.00.00', :neu => true, :firma => 'orangenwerk', :webseite => 'http://orangenwerk.com', :created_at => (Time.now - 14.days)
 
 email_2 = KontaktFormular.create! :anrede => 'Herr', :name => 'fineline-Team', :email => 'cms@orangenwerk.com', :betreff => 'Herrzlich Willkommen', 
-:strasse => 'Stralsunder Str. 20', :plz => '16515', :ort => 'Oranienburg', :land => 'Deutschland', :inhalt => 'Wir begr&uuml;ssen Sie auf Ihrer neuen Seite und w&uuml;nschen viel spass und erfolg damit.
+:strasse => 'Stralsunder Str. 20', :plz => '16515', :ort => 'Oranienburg', :land => 'Deutschland', :inhalt => 'Wir begrüssen Sie auf Ihrer neuen Seite und wünschen viel spass und erfolg damit.
 Sollten Sie fragen oder anregungen zu unserem CMS haben, kontaktieren Sie uns.
 
 orangenwerk
@@ -63,39 +61,39 @@ Stralsunder Str. 20
 info@orangenwerk.com', :user_ip => '00.00.00.00', :neu => true, :firma => 'orangenwerk.com', :webseite => 'http://orangenwerk.com', :created_at => (Time.now - 27.hours)
 
 email_3 = KontaktFormular.create! :anrede => 'Herr', :name => 'fineline-Admin', :email => 'admin@orangenwerk.com', :betreff => 'Aller Anfang ist schwer!', 
-:inhalt => 'Ich begr&uuml;sse Sie auf Ihrer neuen Seite und w&uuml;nschen viel spass und erfolg damit.
+:inhalt => 'Ich begrüsse Sie auf Ihrer neuen Seite und wünschen viel spass und erfolg damit.
 
-Haben Sie bitte keine Ber&uuml;hrungs&auml;ngste, Ihre neue Seite wird Sie nicht beissen und wir haben sie so konzipiert das Sie ohne Vorwarnung nichts unwiederruflich zerst&ouml;ren k&ouml;nnen.
-Ein m&auml;chtiges BackUp-System ist in Arbeit, bis dies jedoch fertig ist sollten Sie folgendes beachten:
- - Sie k&ouml;nnen keine System-Seiten l&ouml;schen (Kontakt, Impressum, Start, ..), dies ist erforderlich um die Seitenfunktion zu gew&auml;hrleisten
- - Seiten welche Sie gel&ouml;scht haben, landen zun&auml;chst im Papierkorb und k&ouml;nnen Wiederhergestellt werden
+Haben Sie bitte keine Berührungsängste, Ihre neue Seite wird Sie nicht beissen und wir haben sie so konzipiert das Sie ohne Vorwarnung nichts unwiederruflich zerstören können.
+Ein mächtiges BackUp-System ist in Arbeit, bis dies jedoch fertig ist sollten Sie folgendes beachten:
+ - Sie können keine System-Seiten löschen (Kontakt, Impressum, Start, ..), dies ist erforderlich um die Seitenfunktion zu gewährleisten
+ - Seiten welche Sie gelöscht haben, landen zunächst im Papierkorb und können Wiederhergestellt werden
 
- - Achtung: Text-Abschnitte sind momentan nicht gesichert, &auml;ndern Sie diese sorgf&auml;ltig! (Sie k&ouml;nnen im Text-Editor je nach Betriebssystem mit [STRG]+[Z] oder mit [cmd]+[Z] R&uuml;ckg&auml;ngig machen.)
+ - Achtung: Text-Abschnitte sind momentan nicht gesichert, ändern Sie diese sorgfältig! (Sie können im Text-Editor je nach Betriebssystem mit [STRG]+[Z] oder mit [cmd]+[Z] Rückgängig machen.)
  
-Ansonsten haben wir versucht alles selbsterkl&auml;rend zu gestallten, drum schauen Sie sich erstmal um und lernen Sie Ihre neue Seite kennen.
+Ansonsten haben wir versucht alles selbsterklärend zu gestallten, drum schauen Sie sich erstmal um und lernen Sie Ihre neue Seite kennen.
 
 
-Sollten Sie Fehler oder st&ouml;rungen entdecken, wenden Sie sich bitte an mich.
+Sollten Sie Fehler oder störungen entdecken, wenden Sie sich bitte an mich.
 
 .. Ihr Administrator: austin [ admin@orangenwerk.com ]', :user_ip => '00.00.00.00', :neu => true, :firma => 'orangenwerk.com', :webseite => 'http://orangenwerk.com', :created_at => (Time.now - 307.minutes)
 
 email_4 = KontaktFormular.create! :anrede => 'Herr', :name => 'fineline Support-Team', :email => 'support@orangenwerk.com', :betreff => 'Sind Sie zufrieden?', 
-:inhalt => 'Wir hoffen Sie sind mit Ihrer neuen Seite zufrieden, sollte mal etwas nicht Ihrer Zufriedenheit entsprechen, geben Sie bescheid, wir sehen was sich tun l&auml;sst.
+:inhalt => 'Wir hoffen Sie sind mit Ihrer neuen Seite zufrieden, sollte mal etwas nicht Ihrer Zufriedenheit entsprechen, geben Sie bescheid, wir sehen was sich tun lässt.
 
 Ihr support-Team.
 support@orangenwerk.com', :user_ip => '00.00.00.00', :neu => true, :firma => 'orangenwerk.com', :webseite => 'http://orangenwerk.com', :created_at => (Time.now - 134.minutes)
 
 email_5 = KontaktFormular.create! :anrede => 'Herr', :name => 'Max Mustermann', :email => 'test_mail@2strange.net', :betreff => 'Test kontaktmail', 
 :inhalt => 'Mustertext:
-&Uuml;berall dieselbe alte Leier. Das Layout ist fertig, der Text l&auml;sst auf sich warten. 
+Überall dieselbe alte Leier. Das Layout ist fertig, der Text lässt auf sich warten. 
 Damit das Layout nun nicht nackt im Raume steht und sich klein und leer vorkommt, 
 springe ich ein: der Blindtext. 
 Genau zu diesem Zwecke erschaffen, immer im Schatten meines großen Bruders »Lorem Ipsum«, 
 freue ich mich jedes Mal, wenn Sie ein paar Zeilen lesen. Denn esse est percipi - 
-Sein ist wahrgenommen werden. Und weil Sie nun schon die G&uuml;te haben, 
-mich ein paar weitere S&auml;tze lang zu begleiten, m&ouml;chte ich diese Gelegenheit nutzen, 
-Ihnen nicht nur als L&uuml;ckenf&uuml;ller zu dienen, sondern auf etwas hinzuweisen, 
-das es ebenso verdient wahrgenommen zu werden: Webstandards n&auml;mlich. Sehen Sie, 
+Sein ist wahrgenommen werden. Und weil Sie nun schon die Güte haben, 
+mich ein paar weitere Sätze lang zu begleiten, möchte ich diese Gelegenheit nutzen, 
+Ihnen nicht nur als Lückenfüller zu dienen, sondern auf etwas hinzuweisen, 
+das es ebenso verdient wahrgenommen zu werden: Webstandards nämlich. Sehen Sie, 
 Webstandards sind das Regelwerk, auf dem Webseiten aufbauen.', :user_ip => '00.00.00.00', :neu => true, :created_at => (Time.now - 13.minutes)
 
 puts 'INFO: erste Mails erstellt.'
