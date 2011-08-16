cd37e1588458469a47836e980da3c65c60d684c8
Austin Strange <web@orangenwerk.com>
2011-08-16 13:44:03 +0200

    Header .. update ... und Admin-CSS

--- strange_header/lib/generators/strange_header/install/templates/create_strange_header.rb
+++ strange_header/lib/generators/strange_header/install/templates/create_strange_header.rb
@@ -4,46 +4,80 @@ class CreateStrangeHeader < ActiveRecord::Migration
   def self.up
     create_table :headers do |t|
       t.string        :name                   # => evtl. Überflüssig
+      
       t.string        :typ                    # => fader  /  slider  /  etc.
       t.string        :stil                   # => fader  /  slider  /  etc.
+      
       t.integer       :breite
       t.integer       :hoehe                  # => höhe des Headers   ... eh. header_height
+      
       t.references    :seite                  # => evtl. Überflüssig
-      # 
-      t.string        :standzeit
-      t.string        :wechelzeit
-      t.string        :sortierung
-      t.text          :hintergrund
-      t.string        :button_set
+
+      t.integer       :staytime               # => Fader / Slider
+      t.integer       :changetime             # => Fader
+            
+      t.boolean       :sort                   # => Sortiert / Zufällig
+      
+      t.boolean       :autoplay               # => Autoplay:  Ja / Nein
+      t.boolean       :shownav                # => Navi:  Ja / Nein
+      t.string        :navstil                # => Auswahl
+      
+      t.string        :button_set             # => Auswahl
+      
+      
+      t.string        :hintergrund            # => Auswahl Hingrund => Farbe / Verlauf / Bild / alles
+      
+      t.string        :farbe1                 # => Standardfarbe für Hintergrund
+      t.string        :farbe2                 # => Farbe2 falls Verlauf 
+      
+      t.string        :bg_pic_uid             # => Hintergrund -Bild/ -Grafik
+      t.string        :bg_pic_cropping        # => möglichst freier Zuschnitt  (zwecks Kacheln)
+      
+      t.string        :bg_pos_h               # => Bild Position horizontal   =>  left / center / right
+      t.string        :bg_pos_v               # => Bild Position vertical   =>   top / center / bottom
+      t.string        :bg_repeat              # => Bild-Wiederholung   =>   keine / horizontal / vertikal / alle Richtung
       
       t.timestamps
     end
     
     create_table :hpics do |t|
-      t.string :name
-      t.text :inhalt
-      t.integer :position
-      t.references :header
-      t.string :bild_uid
-      t.integer :bild_width
-      t.integer :bild_height
-      t.string  :bild_cropping
-      #t.string :bild_file_name
-      #t.string :bild_content_type
-      #t.integer :bild_file_size
-      #t.datetime :bild_updated_at
+      t.string        :name
+      t.string        :titel
+      t.text          :inhalt
+      
+      t.integer       :position
+      
+      # => t.references :header
+      
+      t.string        :bild_uid
+      t.string        :bild_cropping
+
       t.timestamps
     end
     
     create_table :headers_hpics do |t|
-      t.references :hpic
-      t.string :name
-      t.text :inhalt
-      t.integer :position
-      t.references :header
-      t.string  :cropping
+      t.references    :hpic
+      t.references    :header
+      
+      t.string        :titel
+      t.text          :inhalt
+      
+      t.integer       :position
+      
+      t.string        :cropping
+      
       t.timestamps
     end
+    
+    create_table :headers_seiten do |t|
+      t.references    :seite
+      t.references    :header
+      
+      t.timestamps
+    end
+    
+    
+    add_column :seiten, :header_id, :integer
     
   end
 
--- strange_kontaktform/lib/generators/strange_kontaktform/install/templates/create_strange_kontaktform.rb
+++ strange_kontaktform/lib/generators/strange_kontaktform/install/templates/create_strange_kontaktform.rb
@@ -7,15 +7,15 @@ class CreateStrangeKontaktform < ActiveRecord::Migration
       t.string    :vorname
       t.string    :name
       t.string    :firma
-      t.string    :abteiung_oder_position
+      # => t.string    :abteiung_oder_position
       t.string    :email
       t.string    :webseite
       t.string    :strasse
       t.string    :plz
       t.string    :ort
-      t.string    :bundesland
-      t.string    :land
-      t.date      :geburtstag
+      # => t.string    :bundesland
+      # => t.string    :land
+      # => t.date      :geburtstag
       t.string    :telefon
       t.string    :telefax
       t.string    :mobil
@@ -24,15 +24,15 @@ class CreateStrangeKontaktform < ActiveRecord::Migration
       
       t.string    :feld1_name
       t.text      :feld1_wert
-      t.text      :feld1_typ
+      t.string    :feld1_typ
       
       t.string    :feld2_name
       t.text      :feld2_wert
-      t.text      :feld2_typ
+      t.string    :feld2_typ
       
       t.string    :feld3_name
       t.text      :feld3_wert
-      t.text      :feld3_typ
+      t.string    :feld3_typ
       
       
       t.string :user_ip
