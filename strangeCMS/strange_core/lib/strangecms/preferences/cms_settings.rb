2ef4548f4ce0bce9658362d8cdb5e1e064561cd8
Austin Strange <austin@orangenwerk.com>
2011-08-31 02:27:46 +0200

    CMS update

--- strangeCMS/strange_core/lib/strangecms/preferences/cms_settings.rb
+++ strangeCMS/strange_core/lib/strangecms/preferences/cms_settings.rb
@@ -28,7 +28,7 @@ module Strangecms
             
             # => Encoding.default_external = Encoding::UTF_8
             # => Encoding.default_internal = Encoding::UTF_8
-            Encoding.default_external = "UTF-8"
+            # => Encoding.default_external = "UTF-8"
             
             
           rescue
