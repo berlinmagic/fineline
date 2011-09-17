e9ad61fb07dd6f55396bd2d144211c01946b6576
Austin Strange <austin@orangenwerk.com>
2011-09-18 01:50:53 +0200

    new update enginge

--- strangeCMS/lib/generators/strangecms/update_generator.rb
+++ strangeCMS/lib/generators/strangecms/update_generator.rb
@@ -1,8 +1,16 @@
 # encoding: utf-8
+require 'rails/generators/migration'
 module Strangecms
   module Generators
     class UpdateGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+      include Thor::Actions
       desc "Creates a CMS basic structure."
+      
+      def self.next_migration_number(path)
+        @prev_migration_nr ? @prev_migration_nr += 1 : @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        @prev_migration_nr.to_s
+      end
       
       def initial_desc
         puts('   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *')
@@ -26,12 +34,15 @@ module Strangecms
               generate("#{modul.gem_name}:update") unless modul.core?
           end
         end
-     end
-      
-      def migrate_db
-        puts("INFO: erstelle DB-Migration")
-        rake("db:migrate")
       end
+      
+      def update_development_db
+        if yes?("fineline - Development-DB erstellen?")
+          puts("INFO: update DB-Migration")
+          rake("db:migrate")
+        end
+      end
+      
       
       def migrate_db_prod
         puts("INFO: erstelle production DB-Migration")
@@ -49,6 +60,36 @@ module Strangecms
         puts('   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .')
         puts('   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *')
       end
+      
+      private
+        
+        def update_strange_modul( modul )
+          if !!defined?modul.constantize::Engine
+            # => Dir.glob( File.join(File.expand_path("#{ the_path( modul ) }/db/templates", __FILE__), "create*.*") ) do |file|
+            # =>   puts("#{ modul }:: erstelle Migration .. (#{File.basename(file.to_s)})")
+            # =>   migration_template "#{file}", "db/migrate/#{File.basename(file.to_s)}"
+            # => end
+            # migrate Updates
+            Dir.glob( File.join(File.expand_path("#{ the_path( modul ) }/db/templates", __FILE__), "update*.*") ) do |file|
+              puts("#{ modul }:: erstelle Migration update .. (#{File.basename(file.to_s)})")
+              migration_template "#{file}", "db/migrate/#{File.basename(file.to_s)}"
+            end
+            # DB-Seed
+            # => Dir.glob( File.join(File.expand_path("#{ the_path( modul ) }/db/templates", __FILE__), "system_seed*.*") ) do |file|
+            # =>   if File.exist?("db/seeds.rb")
+            # =>     puts("#{ modul }:: erweitere System-Seed .. (#{File.basename(file.to_s)})")
+            # =>     append_to_file "#{Rails.root}/db/seeds.rb",  File.read( "#{ the_path( modul ) }/db/templates/#{File.basename(file.to_s)}" )
+            # =>   else
+            # =>     puts("#{ modul }:: erstelle System-Seed .. (#{File.basename(file.to_s)})")
+            # =>     create_file "#{Rails.root}/db/seeds.rb",  File.read( "#{ the_path( modul ) }/db/templates/#{File.basename(file.to_s)}" )
+            # =>   end
+            # => end
+          end
+        end
+        
+        def the_path( modul )
+          modul.constantize::Engine.config.root
+        end
       
     end
   end
